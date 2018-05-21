91ef2dea326a4b141dccf351a3b224561952d217
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-05-22 00:02:16 +0200

    Update Printing out jagged arrays.cs

--- Printing out jagged arrays.cs
+++ Printing out jagged arrays.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assigning_Jagged_Array {
+namespace Printing_Out_Jagged_Array {
     class Program {
         static void Main(string[] args) {
             int[][] Pyramid = new int[15][];
