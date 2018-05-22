b37406dfdadd6249611b0cb5b132c6828482054d
tomashauser <37981481+tomashauser@users.noreply.github.com>
2018-05-22 21:40:18 +0200

    Update Printing out jagged arrays.cs

--- Printing out jagged arrays.cs
+++ Printing out jagged arrays.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace Printing_Out_Jagged_Array {
     class Program {
         static void Main(string[] args) {
-            int[][] Pyramid = new int[15][];
+            int Number_Of_Arrays;
+            Number_Of_Arrays = 15;
+            int[][] Pyramid = new int[Number_Of_Arrays][];
             Pyramid[00] = new int[] { 75 };
             Pyramid[01] = new int[] { 95, 64 };
             Pyramid[02] = new int[] { 17, 47, 82 };
@@ -23,8 +25,8 @@ namespace Printing_Out_Jagged_Array {
             Pyramid[12] = new int[] { 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48 };
             Pyramid[13] = new int[] { 63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31 };
             Pyramid[14] = new int[] { 04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23 };
-            for (int i = 0; i <= 14; i++) {
-                for (int j = 0; j <= Pyramid[i].Length - 1; j++) {
+            for (int i = 0; i < Number_Of_Arrays; i++) {
+                for (int j = 0; j < Pyramid[i].Length; j++) {
                     Console.Write(Pyramid[i][j] + ", ");
                 }
                 Console.WriteLine("");
